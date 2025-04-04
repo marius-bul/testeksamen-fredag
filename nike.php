63f302e863d6bc52fd9aa70afe573689b9266d9c
marius-bul <mariusbuljo@gmail.com>
2025-04-04 11:30:03 +0200

    redigert vekk alt gucci og redigert det om til nike

--- nike.php
+++ nike.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <link rel="stylesheet" href="nav.css">
     <meta charset="UTF-8">
-    <title>Registrering</title>
+    <title>rating</title>
 </head>
 
 <nav>
@@ -46,8 +46,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    <a href="welcome.php">Hjem</a>
     </div>
     <div class="nav-høyre">
-      <!--<a href="gucci.php">Rate Gucci</a>!-->
-      <a href="nike.php">Rate Nike</a>
+      <a href="gucci.php">Rate Gucci</a>
+      <!--<a href="nike.php">Rate Nike</a>!-->
       <a href="adidas.php">Rate Adidas</a>
     </div>
   </nav>
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   <body>
     <center>
-    <h1>Rate gucci</h1>
+    <h1>Rate Nike</h1>
     </center>
 
 
@@ -68,16 +68,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
          </tr>
 
          <tr>
-            <td> <a href="https://www.interoptik.no/globalassets/kampanje/merkevarefokus/gucci/digital_interoptik-guc-fw22_website_w-3000-x-h-1500px.jpg?transform=DownFit&width=1800"><img src = "https://www.interoptik.no/globalassets/kampanje/merkevarefokus/gucci/digital_interoptik-guc-fw22_website_w-3000-x-h-1500px.jpg?transform=DownFit&width=1800" width="400" height="350" alt="gucci reklame 1"> </a></td>
-            <td> <a href="https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/471409371_1856210638469445_8362207904582359297_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=oPNfixPAZ48Q7kNvwHQ8QGg&_nc_oc=AdlGkh_k2xmmrxHRcbvqFr_6akhJTtChBb7J6nqXZu9liPmdbx7XVe6NFvSxMA-qpYs&_nc_zt=23&_nc_ht=scontent.fsvg1-1.fna&_nc_gid=PB-KEtZ4I6z_Yygtn7dkqQ&oh=00_AYGR5zrEKLi0eBLurLVjpuxrH_GHhGB-GVXiX3NqDhJzqw&oe=67F5563B"><img src = "https://scontent.fsvg1-1.fna.fbcdn.net/v/t39.30808-6/471409371_1856210638469445_8362207904582359297_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=oPNfixPAZ48Q7kNvwHQ8QGg&_nc_oc=AdlGkh_k2xmmrxHRcbvqFr_6akhJTtChBb7J6nqXZu9liPmdbx7XVe6NFvSxMA-qpYs&_nc_zt=23&_nc_ht=scontent.fsvg1-1.fna&_nc_gid=PB-KEtZ4I6z_Yygtn7dkqQ&oh=00_AYGR5zrEKLi0eBLurLVjpuxrH_GHhGB-GVXiX3NqDhJzqw&oe=67F5563B" width="400" height="350" alt="gucci reklame 2"> </a></td>
-            <td> <a href="https://soundvenue.com/wp-content/uploads/2020/09/Gucci-2192x1233.jpg?v=1601461297"><img src = "https://soundvenue.com/wp-content/uploads/2020/09/Gucci-2192x1233.jpg?v=1601461297" width="400" height="350" alt="gucci reklame 3"> </a></td>
+            <td> <a href="https://kampanje.com/globalassets/scanpix/merkevarer/colin-kaepernick-nike-reuters.jpg?format=webp&width=980&quality=90"><img src = "https://kampanje.com/globalassets/scanpix/merkevarer/colin-kaepernick-nike-reuters.jpg?format=webp&width=980&quality=90" width="400" height="350" alt="nike reklame 1"> </a></td>
+            <td> <a href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgajCSW00jr6kOoLxnCxEKrn6HbRjHMU0yQ3FR9wqIvUp2iIbuoR0xWjwL-PPyteB2n-3bgfsP74Pnvd4MGI9kzPzdjFLxvPBUcjqms2vxJGXA5QLFeyRSW0nRMkqfBSK688Mascu1Ovnsm/s1600/Nike.jpg"><img src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgajCSW00jr6kOoLxnCxEKrn6HbRjHMU0yQ3FR9wqIvUp2iIbuoR0xWjwL-PPyteB2n-3bgfsP74Pnvd4MGI9kzPzdjFLxvPBUcjqms2vxJGXA5QLFeyRSW0nRMkqfBSK688Mascu1Ovnsm/s1600/Nike.jpg" width="400" height="350" alt="nike reklame 2"> </a></td>
+            <td> <a href="https://imgproxy.finans.dk/6Vip4M7Wf_GKij_ua9pwVQcVSBu0v3_n8dZwATR3bu8/resize:fill:940:0:0/plain/https%3A%2F%2Fbilleder.finans.dk%2Fpictures%2Fimage%2F12181010%2Fuqt8mw%2FALTERNATES%2Fmaster-3_2%2Fkina"><img src = "https://imgproxy.finans.dk/6Vip4M7Wf_GKij_ua9pwVQcVSBu0v3_n8dZwATR3bu8/resize:fill:940:0:0/plain/https%3A%2F%2Fbilleder.finans.dk%2Fpictures%2Fimage%2F12181010%2Fuqt8mw%2FALTERNATES%2Fmaster-3_2%2Fkina" width="400" height="350" alt="nike reklame 3"> </a></td>
          </tr>
     </table>
     </center>
 
     <center>
-    <p>Rate en reklame om gangen, start teksten med "reklame 1 gucci" når du skal skrive inn på "tilbakemelding" og gjør det på alle 3</p>
-    <form action="gucci.php" method="POST">
+    <p>Rate en reklame om gangen, start teksten med "reklame 1 nike" når du skal skrive inn på "tilbakemelding" og gjør det på alle 3</p>
+    <form action="nike.php" method="POST">
         <input type="text" name="stjerne" placeholder="stjerne 1/5" required>
         <input type="text" name="tilbakemelding" placeholder="tilbakemelding" required>
         <button type="submit">send inn svar</button>
